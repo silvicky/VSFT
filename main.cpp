ef7de758b013f4fa68155d8fc2b95c177383ccc4
Vicky Silviana <vickyisnotaboy@gmail.com>
2022-11-19 22:08:43 +0800

    Improved(implemented) UI?

--- main.cpp
+++ main.cpp
@@ -2,20 +2,19 @@
 #include<fstream>
 #include<vector>
 #include<queue>
+#include<ctime>
 using namespace std;
 struct huffman
 {
     bool isLeaf;
     char content;
     int weight;
-    int size;
     huffman *l,*r;
     huffman()
     {
         isLeaf=1;
         content=0;
         weight=0;
-        size=1;
         l=r=nullptr;
     }
     huffman(char x,int y)
@@ -23,14 +22,12 @@ struct huffman
         isLeaf=1;
         content=x;
         weight=y;
-        size=1;
         l=r=nullptr;
     }
     huffman(huffman *lp,huffman *rp)
     {
         isLeaf=0;
         weight=lp->weight+rp->weight;
-        size=lp->size+rp->size;
         l=lp;
         r=rp;
     }
@@ -45,15 +42,25 @@ struct cmp
 };
 ifstream ifs;
 ofstream ofs;
+long long fileSize=0;
+time_t ta, tb;
 int gf;
 void help(char* myName)
 {
     cout<<"A Huffman coding and decoding program."<<endl;
     cout<<"Usage: "<<endl;
-    cout<<myName<<" c <in> <out> - Code"<<endl;
-    cout<<myName<<" d <in> <out> - Decode"<<endl;
+    cout<<"huffman c <in> <out> - Code"<<endl;
+    cout<<"huffman d <in> <out> - Decode"<<endl;
     cout<<"Use at your own risk!"<<endl;
     cout<<"Author: Vicky Silviana"<<endl;
+}
+void printProgress(int val)
+{
+    cout << "\r[";
+    for (int i = 0; i < val/5; i++)cout << "#";
+    for (int i = val/5; i < 20; i++)cout << ".";
+    cout << "](" << val << "%)";
+    fflush(stdout);
 }
 /* Recursively travel through the tree.
  *
@@ -79,11 +86,12 @@ void travel(huffman* cur,vector<bool>* vec,vector<bool>& stk,int fa)
     stk.pop_back();
 }
 /* Get more bits from ifs. */
-void feed(queue<bool>& q)
+bool feed(queue<bool>& q)
 {
     char c;
-    ifs.get(c);
+    if (!ifs.get(c))return false;
     for(int i=7;i>=0;i--)q.push((c>>i)%2);
+    return true;
 }
 void coding()
 {
@@ -93,7 +101,9 @@ void coding()
     vector<bool> vec[256];
     vector<bool> stk;
     queue<bool> oq;
-    long long val=0;
+    time(&ta);
+    long long val=0,curSize=0,finalSize=0;
+    cout << "Preflight...\n";
     while(ifs.get(tmpc))cnt[tmpc+128]++;
     priority_queue<huffman*,vector<huffman*>,cmp> pq;
     /* Build Huffman tree. */
@@ -116,12 +126,16 @@ void coding()
     val=0;
     for(int i=0;i<256;i++)val+=cnt[i];
     for(int i=7;i>=0;i--)ofs<<(char)(val>>(i*8)%256);
+    fileSize = val;
     ifs.clear();
     ifs.seekg(0,ios::beg);
     val=0;
     /* Write data. */
+    printProgress(0);
     while(ifs.get(tmpc))
     {
+        curSize++;
+        if (curSize * 100 / fileSize != (curSize - 1) * 100 / fileSize)printProgress(curSize*100/fileSize);
         for(int i=0;i<vec[tmpc+128].size();i++)oq.push(vec[tmpc+128][i]);
         while(oq.size()>=8)
         {
@@ -133,6 +147,7 @@ void coding()
                 oq.pop();
             }
             ofs<<(char)val;
+            finalSize++;
         }
     }
     /* Force write a byte to store data at the end. */
@@ -146,24 +161,39 @@ void coding()
         oq.pop();
     }
     ofs<<(char)val;
+    finalSize++;
+    printProgress(100);
+    time(&tb);
+    cout << "\nOperation done in "<<tb-ta<<"s with compression rate " << finalSize*100 / fileSize << "%";
 }
 void decoding()
 {
-    char head[3],tmpc,ta,tb;
+    char head[3],tmpc,tmpa,tmpb;
     huffman *node[512],*cur;
-    long long fsize;
+    long long fileSize;
     int fa;
     queue<bool> q;
+    time(&ta);
     /* Read file header. */
     for(int i=0;i<3;i++)ifs.get(head[i]);
+    if(head[0]!='V'||head[1]!='S'||head[2]!='Z')
+    {
+        cout << "FATAL: Invalid header!";
+        return;
+    }
     for(int i=0;i<511;i++)
     {
         ifs.get(tmpc);
-        ifs.get(ta);
-        ifs.get(tb);
+        ifs.get(tmpa);
+        ifs.get(tmpb);
         node[i]=new huffman(tmpc,0);
-        if(ta>1)continue;
-        fa=(ta>=0?ta:ta+256)*256+(tb>=0?tb:tb+256);
+        if(tmpa>1)continue;
+        fa=(tmpa>=0?tmpa:tmpa+256)*256+(tmpb>=0?tmpb:tmpb+256);
+        if((fa>512&&fa!=1024)||!node[fa])
+        {
+            cout << "FATAL: Invalid tree structure!";
+            return;
+        }
         if(node[fa]->isLeaf)
         {
             node[fa]->isLeaf=0;
@@ -172,25 +202,37 @@ void decoding()
         else node[fa]->r=node[i];
     }
     cur=node[0];
-    fsize = 8;
+    fileSize = 0;
     for(int i=0;i<8;i++)
     {
         ifs.get(tmpc);
-        fsize<<=8;
-        fsize+=(tmpc>=0?tmpc:tmpc+256);
+        fileSize<<=8;
+        fileSize+=(tmpc>=0?tmpc:tmpc+256);
     }
     /* Restore file. */
-    for(long long i=0;i<fsize;i++)
+    printProgress(0);
+    for(long long i=0;i<fileSize;i++)
     {
-        while(!cur->isLeaf)
+        if (i * 100 / fileSize != (i - 1) * 100 / fileSize)printProgress(i * 100 / fileSize);
+        while (!cur->isLeaf)
         {
-            if(q.empty())feed(q);
+            if (q.empty())
+            {
+                if(!feed(q))
+                {
+                    cout << "\nFATAL: EOF before correct position!("<<i<<" vs "<<fileSize<<")\nFile damaged.";
+                    return;
+                }
+            }
             cur=q.front()?cur->r:cur->l;
             q.pop();
         }
         ofs<<cur->content;
         cur=node[0];
     }
+    printProgress(100);
+    time(&tb);
+    cout << "\nOperation done in " << tb - ta << "s";
 }
 int main(int argc, char** argv)
 {
